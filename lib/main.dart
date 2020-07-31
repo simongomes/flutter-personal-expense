a7b3c40bdb9ead14fecef9115f202f8286b5c532
Simon Gomes <busy.s.simon@gmail.com>
2020-07-31 22:39:31 +0600

    NewTransaction widget converted to a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('PERSONAL EXPENSE'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTransaction;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
 
-    if( enteredTitle.isEmpty || enteredAmount <= 0 ) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
 
-    addTransaction(titleController.text, double.parse(amountController.text));
+    widget.addTransaction(
+        titleController.text,
+        double.parse(amountController.text)
+    );
+
+    Navigator.of(context).pop();
   }
 
   @override
@@ -39,10 +50,9 @@ class NewTransaction extends StatelessWidget {
               onSubmitted: (_) => submitData(),
             ),
             FlatButton(
-              child: Text('Add Transaction'),
-              textColor: Colors.purple,
-              onPressed: submitData
-            )
+                child: Text('Add Transaction'),
+                textColor: Colors.purple,
+                onPressed: submitData)
           ],
         ),
       ),
