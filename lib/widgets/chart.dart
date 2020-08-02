0747fa5b8ddecf6d2f83fd181e36ae11140b4c09
Simon Gomes <busy.s.simon@gmail.com>
2020-08-02 20:24:12 +0600

    Chart Widget and initial logic added

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      var totalSum = 0.0;
+
+      for(var i = 0; i < recentTransactions.length; i++) {
+        if(recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year
+        ) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E(weekDay),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
