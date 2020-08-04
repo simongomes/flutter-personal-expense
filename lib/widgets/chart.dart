add7e9722f375e72d4287e9e8b707b81fbc69faa
Simon Gomes <busy.s.simon@gmail.com>
2020-08-04 21:12:09 +0600

    basic chart ui added

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import './chart_bar.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
@@ -24,9 +24,15 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold( 0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -36,9 +42,13 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[
-          Text('I am chart'),
-        ],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: groupedTransactionValues.map((transaction) {
+          return ChartBar(
+              transaction['day'],
+              transaction['amount'],
+              totalSpending == 0.0 ? 0.0 : (transaction['amount'] as double) / totalSpending);
+        }).toList(),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentageOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text("\$${spendingAmount.toStringAsFixed(0)}"),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                  heightFactor: spendingPercentageOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
