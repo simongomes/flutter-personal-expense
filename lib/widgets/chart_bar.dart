9c06f90af003f8a744c72caaad00595282aff189
Simon Gomes <busy.s.simon@gmail.com>
2020-08-06 20:40:41 +0600

    Transaction list ui update and some fine tuning on chart section

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,7 +11,12 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text("\$${spendingAmount.toStringAsFixed(0)}")),
+        Container(
+            height: 20,
+            child: FittedBox(
+                child: Text("\$${spendingAmount.toStringAsFixed(0)}")
+            )
+        ),
         Container(
           height: 60,
           width: 10,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -34,41 +34,26 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                              child: Text("\$${transactions[index].amount}"
+                              )
+                          )
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(transactions[index].title,
-                              style: Theme.of(context).textTheme.headline6),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
                   ),
-                );
+                ); 
               },
               itemCount: transactions.length,
             ),
